cae792f84f03a1489abf0327c44924a8368c76bb
Obi.M <mail@obimadu.pro>
2023-10-07 03:24:08 +0100

    Define Config struct & func(*Config).routes()

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,7 @@
 package main
 
+type Config struct{}
+
 func main() {
 	//
 }
---
+++ cmd/api/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	// Define the router
+	mux := chi.NewRouter()
+
+	// Define connection constraints
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	// Define health check
+	mux.Use(middleware.Heartbeat("/health"))
+
+	return mux
+}
