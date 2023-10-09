4fb0525ac45e63d8fa3b7c889609fabc9972c305
Obi.M <mail@obimadu.pro>
2023-10-09 18:00:44 +0100

    Write create and start http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type Config struct{}
 
 func main() {
-	//
+	app := Config{}
+
+	// Create server
+	srv := &http.Server{
+		Addr:    ":80",
+		Handler: app.routes(),
+	}
+
+	// Start server
+	fmt.Printf("** Listening on port 80 **")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
 }
